b463a5ca4c2c28ddf88d0df0bd75ff9aadba2bd1
amritsingh2022 <69254880+amritsingh2022@users.noreply.github.com>
2020-08-11 23:42:00 +0530

    Update QuestionsTest.java

--- src/test/java/fibbase/QuestionsTest.java
+++ src/test/java/fibbase/QuestionsTest.java
@@ -30,5 +30,21 @@ public class QuestionsTest {
 	public void test5() {
 		assertEquals(true, Questions.trueorfalse(13));
 	}
+		@Test
+	public void test6() {
+		assertEquals(4, Questions.findgcd(12, 16));
+	}
+	@Test
+	public void test7() {
+		String a1 = "Harry Potter was a highly unusual boy in many ways. For one thing, he hated the summer holidays more than any other time of year. For another, he really wanted to do his homework, but was forced to do it in secret, in the dead of the night. And he also happened to be a wizard."
+		int[] a = {382, 7}
+		assertEquals(a, Questions.averagestring(a1));
+	}
+	@Test
+	public void test8() {
+		String a1 = "HOW DO YOU DO?"
+		String a = "DO DO HOW YOU"
+		assertEquals(a, Questions.potential(a1));
+	}
 
 }
